d60cb6fe00a913127e375461ea3f018c0a6658b2
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-02-06 21:09:48 +0800

    Constant

---
+++ Constant/Constant.php
@@ -0,0 +1,17 @@
+<?php
+
+// Constant adalah tempat untuk menyimpan data yang tidak bisa dirubah lagi setelah di deklarasikan.
+// bersifat immutable (tidak bisa dirubah)
+
+define("NAMA", "Hendra Usman");
+define("UMUR", 18);
+
+echo "Nama : ";
+echo NAMA;
+echo "\n";
+
+echo "Umur : ";
+echo UMUR;
+echo "\n";
+
+?>
