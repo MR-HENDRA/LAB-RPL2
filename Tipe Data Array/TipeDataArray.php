17d298c39fab0c903bfbb613f6b3c83cbbf83b82
Hendra Usman <96562868+MR-HENDRA@users.noreply.github.com>
2022-02-07 23:02:24 +0800

    Update TipeDataArray.php

--- Tipe Data Array/TipeDataArray.php
+++ Tipe Data Array/TipeDataArray.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #---> 1. Array adalah tipe data yang berisikan kosong atau banyak data. 
 #---> 2. Array di PHP bisa berisikan data dengan jenis berbeda-beda.
 #---> 3. Array di PHP memiliki panjang dinamis, artinya kita bisa menambah data ke Array
