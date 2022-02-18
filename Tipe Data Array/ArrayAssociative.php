0200a2e6d3378e1da0921d2d5e6cbe25676b906f
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-02-18 22:55:31 +0800

    menambahkan gambar

--- Tipe Data Array/ArrayAssociative.php
+++ Tipe Data Array/ArrayAssociative.php
@@ -8,14 +8,16 @@ $mahasiswa = [
         "nama" => "Hendra Usman",
         "NIM" => "D0221079",
         "jurusan" => "Teknik Informatika",
-        "email" => "hendra@gmail.com"
+        "email" => "hendra@gmail.com",
+        "gambar" => "hndra.jpeg"
 
     ],
     [
         "nama" => "Fyan Ramadhan",
         "NIM" => "D0221085",
         "jurusan" => "Ilmu Komputer",
-        "email" => "fyan@gmail.com"
+        "email" => "fyan@gmail.com",
+        "gambar" => "fyan.jpeg"
     ]]
 
 ?>
@@ -24,12 +26,19 @@ $mahasiswa = [
 <html>
 <head>
     <title>Daftar Mahasiswa</title>
+    <style type="text/css">
+        IMG {
+            width: 100px;
+            height: 100px;
+        }
+    </style>
 </head>
 <body>
     <h1>Daftar Mahasiswa</h1>
 
     <?php foreach($mahasiswa as $mhs): ?>
         <ul>
+            <li id="IMG"><IMG src="IMG/<?= $mhs["gambar"] ?>"></li>
             <li>Nama : <?= $mhs["nama"]?></li>
             <li>NIM : <?= $mhs["NIM"]?></li>
             <li>Jurusan: <?= $mhs["jurusan"]?></li>
